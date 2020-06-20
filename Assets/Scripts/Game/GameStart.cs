566ff90b8637fd6d7c888c8fb68e8b3b92a4b168
unknown <bradur@github.com>
2020-06-21 00:15:09 +0300

    Activate tower when it gets energy.

--- Assets/Scripts/Game/GameStart.cs
+++ Assets/Scripts/Game/GameStart.cs
@@ -14,6 +14,8 @@ public class GameStart : MonoBehaviour
 
         Tower tower = Prefabs.Instantiate<Tower>();
         tower.transform.position = new Vector3(5f, 0f, 5f);
+        Tower tower2 = Prefabs.Instantiate<Tower>();
+        tower2.transform.position = new Vector3(7f, 0f, 7f);
 
         GameObject goblin = Prefabs.Get<Goblin>();
         for (int i = 0; i < 10; i += 1) {
