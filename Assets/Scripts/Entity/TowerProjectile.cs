c8b8b49f701dc51e9c3f9d2cca4ba86ed34f1b20
Juutis <jutkula@gmail.com>
2020-06-21 14:58:28 +0300

    LIGHTNING!

--- Assets/Scripts/Entity/TowerProjectile.cs
+++ Assets/Scripts/Entity/TowerProjectile.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class TowerProjectile : MonoBehaviour
 {
-    private Goblin target;
+    [SerializeField]
     private float duration = 0.5f;
+
+    private Goblin target;
     private float minDistance = 0.01f;
     private int damage = 1;
     private float timer = 0f;
     private Vector3 startPos;
+
+    bool damaged = false;
     public void Launch(Goblin targetGoblin)
     {
         target = targetGoblin;
         startPos = transform.position;
+        damaged = false;
     }
 
     void Update()
@@ -24,9 +29,10 @@ public class TowerProjectile : MonoBehaviour
             Vector3 targetPos = target.transform.position;
             targetPos.y = 0.5f;
             transform.position = Vector3.Lerp(startPos, targetPos, timer / duration);
-            if (Vector3.Distance(transform.position, targetPos) < minDistance) {
+            if (Vector3.Distance(transform.position, targetPos) < minDistance && !damaged) {
                 target.TakeDamage(damage);
-                Die();
+                Invoke("Die", 0.5f);
+                damaged = true;
             }
         }
     }
