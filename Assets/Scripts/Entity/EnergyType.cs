f979ca0d7969550865ccc58c4fba36f9ac891e11
Matias Pitkänen <M2tias@GMail.com>
2020-06-21 13:09:39 +0300

    Energy type configs and changing the energy crystal material

--- Assets/Scripts/Entity/EnergyType.cs
+++ Assets/Scripts/Entity/EnergyType.cs
@@ -6,12 +6,16 @@ public class EnergyType : MonoBehaviour
 {
     [SerializeField]
     EnergyTypes type;
+    [SerializeField]
+    MeshRenderer crystalRenderer;
     Material crystalMaterial;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        EnergyTypeConfig conf = Configs.main.EnergyTypeConfigs[type];
+        crystalMaterial = conf.CrystalMaterial;
+        crystalRenderer.material = crystalMaterial;
     }
 
     // Update is called once per frame
--- Assets/Scripts/ScriptableObjects/EnergyTypeConfig.cs
+++ Assets/Scripts/ScriptableObjects/EnergyTypeConfig.cs
@@ -5,5 +5,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EnergyTypeConfig", menuName = "New EnergyTypeConfig")]
 public class EnergyTypeConfig : ScriptableObject
 {
+    [SerializeField]
+    private EnergyTypes type;
+    public EnergyTypes Type { get { return type; } }
+
+    [SerializeField]
+    private Material crystalMaterial;
+    public Material CrystalMaterial { get { return crystalMaterial; } }
 
 }
