805470f1c6dce47ff9d27eec884e34001b7e602f
unknown <bradur@github.com>
2020-06-21 18:43:25 +0300

    Minimap clamping.

--- Assets/MinimapDraggable.cs
+++ Assets/MinimapDraggable.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class MinimapDraggable : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler, IDragHandler, IEndDragHandler
+public class MinimapDraggable :
+    MonoBehaviour,
+    IPointerClickHandler,
+    IPointerDownHandler,
+    IPointerUpHandler,
+    IDragHandler,
+    IEndDragHandler
 {
 
     [SerializeField]
     private RectTransform miniMapControl;
     private Transform cameraTransform;
 
+    private float speed = 55f;
     private LayerMask mask;
 
     private Vector2 prevClickPos;
 
     private bool pointerDown = false;
+    private float border = 20f;
+
 
     void Start()
     {
@@ -23,41 +32,105 @@ public class MinimapDraggable : MonoBehaviour, IPointerClickHandler, IPointerDow
         mask = LayerMask.GetMask("Water");
     }
 
-    void Update() {
-        Vector2 dir = GetDir(prevClickPos);
-        if (dir.magnitude > 0.01f && pointerDown) {
-            cameraTransform.Translate(dir * 100, Space.Self);
-        } else {
+    void Update()
+    {
+        Vector2 mousePanDir = GetMousePanDir();
+        Vector2 mouseDragDir = GetDir(GetMouseonMinimap(prevClickPos));
+        if (mousePanDir.magnitude > 0.01f)
+        {
+            //mousePanDir = GetDir(mousePanDir);
+            cameraTransform.Translate(mousePanDir * Time.deltaTime * speed, Space.Self);
+        }
+        else
+        {
+            if (mouseDragDir.magnitude > 0.01f && pointerDown)
+            {
+                cameraTransform.Translate(mouseDragDir * 1000f * Time.deltaTime, Space.Self);
+            }
+        }
+        if (mouseDragDir.magnitude < 0.01f && pointerDown)
+        {
             pointerDown = false;
         }
     }
 
-    private Vector2 GetDir(Vector2 mousePos) {
-        Vector2 mouseOnMiniMap = new Vector2(
-            (mousePos.x - 20) / 200f,
-            (mousePos.y - 20) / 200f
-        );
+    private Vector2 GetMousePanDir()
+    {
+        Vector2 dir = Vector2.zero;
+        Vector2 mous = Input.mousePosition;
         Vector2 minimapControlPos = new Vector2(
             (miniMapControl.anchoredPosition.x + miniMapControl.sizeDelta.x / 2) / 200f,
             (miniMapControl.anchoredPosition.y + miniMapControl.sizeDelta.y / 2) / 200f
         );
-        return mouseOnMiniMap - minimapControlPos;
+        
+        if (mous.x > Screen.width / 2 && minimapControlPos.x <= 1)
+        {
+            if (mous.x > Screen.width || Mathf.Abs(Screen.width - mous.x) <= border)
+            {
+                dir.x = 1f;
+            }
+        }
+        else if (mous.x <= border && minimapControlPos.x >= 0)
+        {
+            dir.x = -1f;
+        }
+        if (mous.y > Screen.height / 2 && minimapControlPos.y <= 1)
+        {
+            if (mous.y > Screen.height || Mathf.Abs(Screen.height - mous.y) <= border)
+            {
+                dir.y = 1f;
+            }
+        }
+        else if (mous.y <= border && minimapControlPos.y >= 0)
+        {
+            dir.y = -1f;
+        }
+        return dir;
+    }
+    private Vector2 GetMouseonMinimap(Vector2 mousePos)
+    {
+        Vector2 mouseOnMiniMap = new Vector2(
+            (mousePos.x - 20) / 200f,
+            (mousePos.y - 20) / 200f
+        );
+        return mouseOnMiniMap;
     }
 
-    public void OnDrag(PointerEventData data) {
+    private Vector2 GetDir(Vector2 mouseOnMiniMap) {
+        Vector2 minimapControlPos = new Vector2(
+            (miniMapControl.anchoredPosition.x + miniMapControl.sizeDelta.x / 2) / 200f,
+            (miniMapControl.anchoredPosition.y + miniMapControl.sizeDelta.y / 2) / 200f
+        );
+        mouseOnMiniMap = new Vector2(
+            Mathf.Clamp(mouseOnMiniMap.x, 0f, 1f),
+            Mathf.Clamp(mouseOnMiniMap.y, 0f, 1f)
+        );
+        minimapControlPos = new Vector2(
+            Mathf.Clamp(minimapControlPos.x, 0f, 1f),
+            Mathf.Clamp(minimapControlPos.y, 0f, 1f)
+        );
+        Vector2 ret = (mouseOnMiniMap - minimapControlPos);
+        return ret;
+    }
+
+    public void OnDrag(PointerEventData data)
+    {
         pointerDown = true;
         prevClickPos = data.position;
     }
 
-   public void OnEndDrag(PointerEventData data) {
+    public void OnEndDrag(PointerEventData data)
+    {
         pointerDown = false;
         prevClickPos = data.position;
     }
 
-    public void OnPointerUp(PointerEventData pointerEventData) {
+    public void OnPointerUp(PointerEventData pointerEventData)
+    {
         pointerDown = false;
     }
-    public void OnPointerDown(PointerEventData pointerEventData) {
+    public void OnPointerDown(PointerEventData pointerEventData)
+    {
         pointerDown = true;
         prevClickPos = pointerEventData.position;
     }
---
+++ Assets/Scripts/UI/UIMouseZoom.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMouseZoom : MonoBehaviour
+{
+    private Transform cameraTransform;
+
+
+    private float scrollSpeed = 30f;
+    private float scrollMin = 5f;
+    private float scrollMax = 11f;
+
+
+
+    private Camera mapCam;
+    private LayerMask mask;
+
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        mapCam = cameraTransform.GetComponentInChildren<Camera>();
+        mask = LayerMask.GetMask("Water");
+    }
+
+    void Update()
+    {
+        if (Mathf.Abs(Input.mouseScrollDelta.y) > 0.02f)
+        {
+            Camera.main.orthographicSize = Mathf.Clamp(
+                Camera.main.orthographicSize + -Input.mouseScrollDelta.y * scrollSpeed * Time.deltaTime,
+                scrollMin,
+                scrollMax
+            );
+        }
+    }
+}
