dd486c8a19e0ea3ce4bf909e311048be6f1ac454
unknown <bradur@github.com>
2020-06-22 16:08:38 +0300

    Energy UI.

---
+++ Assets/Scripts/UI/ChosenEnergyIndicator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChosenEnergyIndicator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private EnergyTypes energyType;
+
+    public EnergyTypes Type { get { return energyType; } }
+
+    [SerializeField]
+    private Color activeColor;
+    private Color originalImgColor;
+    private Color originalTxtColor;
+    private Image imgHighlight;
+    private Text txtHighlight;
+
+    void Start()
+    {
+        imgHighlight = this.FindChildObject("source").GetComponent<Image>();
+        txtHighlight = GetComponentInChildren<Text>();
+        originalImgColor = imgHighlight.color;
+        originalTxtColor = txtHighlight.color;
+    }
+    public void Activate()
+    {
+        imgHighlight.color = activeColor;
+        txtHighlight.color = activeColor;
+    }
+
+    public void Deactivate()
+    {
+        imgHighlight.color = originalImgColor;
+        txtHighlight.color = originalTxtColor;
+    }
+}
