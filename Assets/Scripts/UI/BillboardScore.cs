a1e267a29eb7ccf562f82a0e28e9d1df9ef3162e
unknown <bradur@github.com>
2020-06-22 18:17:32 +0300

    Hovering Score text. + Extra.

---
+++ Assets/Scripts/UI/BillboardScore.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BillboardScore : MonoBehaviour
+{
+
+    private Transform camTransform;
+    private Quaternion originalRotation;
+    private RectTransform rt;
+
+    [SerializeField]
+    private Text txtScore;
+
+    [SerializeField]
+    private Animator animator;
+
+    void Start()
+    {
+        rt = GetComponent<RectTransform>();
+        originalRotation = transform.rotation;
+        camTransform = Camera.main.transform;
+    }
+
+    public void Initialize(Vector3 pos, int score)
+    {
+        txtScore.text = score + "";
+        transform.SetParent(UIManager.main.WorldSpaceCanvas);
+        transform.position = pos;
+        animator.SetTrigger("Show");
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        transform.rotation = originalRotation * camTransform.rotation;
+    }
+
+}
