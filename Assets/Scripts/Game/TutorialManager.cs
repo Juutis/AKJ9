dd852763c33782bd7bc760d24ddfc0dcf39f99e5
unknown <bradur@github.com>
2020-06-22 15:05:40 +0300

    UI Shizzzzzzz.

--- Assets/Scripts/Game/TutorialManager.cs
+++ Assets/Scripts/Game/TutorialManager.cs
@@ -12,6 +12,17 @@ public class TutorialManager : MonoBehaviour
     private int currentStep = 0;
     GenericDialog dialog;
 
+    public static TutorialManager main;
+
+    void Awake()
+    {
+        main = this;
+    }
+
+    public void StartTutorial() {
+        started = true;
+    }
+    private bool started = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,31 +32,34 @@ public class TutorialManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(currentStep < steps.Count && dialog == null)
+        if (started)
         {
-            TutorialStep step = steps[currentStep];
+            if (currentStep < steps.Count && dialog == null)
+            {
+                TutorialStep step = steps[currentStep];
 
-            dialog = Prefabs.Instantiate<GenericDialog>();
-            dialog.Initialize(step.title, step.description, step.dialogTarget);//, new Vector2(300, 300));
-        }
+                dialog = Prefabs.Instantiate<GenericDialog>();
+                dialog.Initialize(step.title, step.description, step.dialogTarget);//, new Vector2(300, 300));
+            }
 
-        if(dialog != null)
-        {
-            dialog.UpdatePosition();
-        }
+            if (dialog != null)
+            {
+                dialog.UpdatePosition();
+            }
 
-        if (Input.GetKeyDown(KeyCode.Space) && dialog != null)
-        {
-            dialog.Hide();
-            currentStep++;
-            dialog = null;
-        }
+            if (Input.GetKeyDown(KeyCode.Space) && dialog != null)
+            {
+                dialog.Hide();
+                currentStep++;
+                dialog = null;
+            }
 
-        if(Input.GetKeyDown(KeyCode.Return))
-        {
-            dialog.Hide();
-            dialog = null;
-            currentStep = steps.Count;
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                dialog.Hide();
+                dialog = null;
+                currentStep = steps.Count;
+            }
         }
     }
 }
---
+++ Assets/Scripts/UI/UIMenu.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject container;
+    private GameObject startGameButton;
+    private GameObject restartGameButton;
+    private GameObject continueGameButton;
+    private GameObject exitGameButton;
+    private GameObject tutorialButton;
+    private Text txtMessage;
+    private bool initialized = false;
+
+    public void Show(string message, bool showContinue, bool showStart, bool showRestart, bool showExit, bool showTutorial)
+    {
+        container.SetActive(true);
+        if (!initialized) {
+            txtMessage = this.FindChildObject("message").GetComponent<Text>();
+            tutorialButton = this.FindChildObject("TutorialButton").gameObject;
+            startGameButton = this.FindChildObject("StartGameButton").gameObject;
+            restartGameButton = this.FindChildObject("RestartButton").gameObject;
+            continueGameButton = this.FindChildObject("ContinueButton").gameObject;
+            exitGameButton = this.FindChildObject("ExitButton").gameObject;
+            initialized = true;
+        }
+        txtMessage.text = message;
+        continueGameButton.SetActive(showContinue);
+        tutorialButton.SetActive(showTutorial);
+        startGameButton.SetActive(showStart);
+        restartGameButton.SetActive(showRestart);
+        exitGameButton.SetActive(showExit);
+    }
+
+    public void Hide()
+    {
+        container.SetActive(false);
+        UIManager.main.MenuWasClosed();
+    }
+    public void StartGame()
+    {
+        Debug.Log("Start");
+        Hide();
+    }
+
+    public void StartTutorial()
+    {
+        Debug.Log("Tutorial");
+        Hide();
+        TutorialManager.main.StartTutorial();
+    }
+    public void Continue()
+    {
+        Debug.Log("Continue");
+        Hide();
+    }
+    public void Restart()
+    {
+        Debug.Log("Restart");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Hide();
+    }
+    public void Exit()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+        Hide();
+    }
+}
